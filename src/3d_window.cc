ef89fcfa1caa75000d43c4cc8eda00a9c9fbc1ac
Martin Pfeiffer <martinp.dev@googlemail.com>
2011-06-04 16:59:35 +0200

    added glew

--- src/3d_window.cc
+++ src/3d_window.cc
@@ -12,7 +12,10 @@
 #include <OpenGL/gl.h>
 #include <OpenGL/glext.h>
 #else
-#include <GL/freeglut.h>
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <GL/glext.h>
 #endif
 
 #include "types.h"
